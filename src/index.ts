6be44569ae84ae3318bba06d2575c76111c7e56f
Noah Allen <noahtallen@gmail.com>
2020-10-25 16:53:16 -0700

    Tie it all together

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,47 @@
-import dotenv from 'dotenv';
+// External dependencies:
+import express from 'express';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
 
-// Loads .env values into process.env
+// Load config values.
+import * as dotenv from 'dotenv';
 dotenv.config();
+
+// Internal dependencies:
+import { setupDatabase } from './utils/database';
+import errorMiddleware from './middleware/errors';
+import authRouter from './routes/auth';
+
+const port = process.env.PORT;
+if (!port) {
+	throw new Error('Please specify a port to run the server on.');
+}
+
+const server = express();
+
+// 1. Setup database:
+setupDatabase();
+
+// 2. Setup express middleware:
+server.use(
+	cors({
+		origin: `http://localhost:${port}`,
+	}),
+);
+
+server.use(bodyParser.json());
+
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.use(errorMiddleware);
+
+// 3. Setup express routes:
+server.get('/', (req, res) => {
+	res.json({ message: 'welcome!' });
+});
+
+server.listen(port, () => {
+	console.log(`server is running on port ${port}`);
+});
+
+server.use('/auth', authRouter);
